e1f78e86885ce2be8849cd427a0780ae473bd025
Christopher S. Aondona <christohybrid185@gmail.com>
2026-06-21 06:49:09 +0100

    chore: commit any remaining untracked agent or workspace files

--- src/routes/_home/index.tsx
+++ src/routes/_home/index.tsx
@@ -3,7 +3,6 @@ import { Services } from "~/components/services";
 import { Contact } from "~/components/contact";
 
 export default function Home() {
-  throw new Error("Not implemented");
 
   return (
     <>
